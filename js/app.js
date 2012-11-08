bdc261777267a8f09ca73c96a5f47c8d080760ac
Laura McCartney <hello@lauramccartney.co.uk>
2012-11-08 13:44:26 +0000

    Make background image change colour to match image

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*Makes background image change when refreshed*/
 function changeImg(imgNumber) {
 	var myImages = ["../img/background_tile.png", "../img/background_tile_blue.png", "../img/background_tile_green.png", "../img/background_tile_purple.png"]; 
+	var myColors = ["#d1261e", "#6167e6", "#3db322", "#a12cbb"];
 	var newImgNumber =Math.floor(Math.random()*myImages.length);
 	document.body.style.backgroundImage = 'url('+myImages[newImgNumber]+')';
+	document.body.style.backgroundColor = myColors[newImgNumber];
 }
 window.onload=changeImg;
