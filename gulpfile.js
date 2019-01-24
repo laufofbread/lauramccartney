72f64fe7d3f03f4605a3015b3571d6042ed885d9
Laura McCartney <hello@lauramccartney.co.uk>
2019-01-24 16:00:32 +0000

    Update everything and fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /*!
  * gulp
- * $ npm install gulp-concat gulp-uglify gulp-rename gulp-ruby-sass gulp-minify-css gulp-notify gulp-autoprefixer gulp-livereload gulp-watch gulp-imagemin gulp-cache --save-dev
+ * $ npm install gulp-concat gulp-uglify gulp-rename gulp-ruby-sass gulp-clean-css gulp-notify gulp-autoprefixer gulp-livereload gulp-watch gulp-imagemin gulp-cache --save-dev
 */
 
 var gulp = require('gulp'),
@@ -8,14 +8,14 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-ruby-sass'),
-	minifycss = require('gulp-minify-css'),
+	minifycss = require('gulp-clean-css'),
 	notify = require('gulp-notify'),
 	autoprefixer = require('gulp-autoprefixer'),
 	livereload = require('gulp-livereload'),
 	watch = require('gulp-watch'),
 	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache')
-	
+	cache = require('gulp-cache');
+
 
 gulp.task('scripts', function() {
     return gulp.src([
@@ -54,19 +54,25 @@ gulp.task('images', function() {
 gulp.task('watch', function() {
 
 	// Watch .scss files
-	gulp.watch('css/*.scss', ['styles']);
+	gulp.watch('css/*.scss', gulp.parallel(['styles']));
 
 	// Watch .js files
-	gulp.watch('js/**/*.js', ['scripts']);
-	
+	gulp.watch('js/**/*.js', gulp.parallel(['scripts']));
+
 
 	// Create LiveReload server
 	livereload.listen();
 
 	// Watch any files in dist/, reload on change
-	gulp.watch(['css/style.min.css, js/app.min.js']).on('change', livereload.changed);
+	gulp.watch(['css/style.min.css, js/app.min.js'])
+	.on('change', function(path, stats) {
+        console.log(path);
+        livereload.changed
+    })
 
 });
 
 
-gulp.task('default', ['watch', 'scripts', 'styles', 'images']);
+gulp.task('default', gulp.series(['scripts', 'styles', 'images', 'watch'], function(done) {
+	done();
+}));
