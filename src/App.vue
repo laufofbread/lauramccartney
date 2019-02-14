bb20d4314e9668e113877cce5fbbf8c2c3357051
Laura McCartney <hello@lauramccartney.co.uk>
2019-02-14 13:52:28 +0000

    Add new views

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>
+      <a href="/">Laura McCartney</a>
+    </h1>
+    <ul class="nav">
+      <li>
+        <router-link to="/inherited-dysfunction/">Inherited dysfunction</router-link>
+      </li>
+      <li>
+        <router-link to="/wood-ash/">Wood ash</router-link>
+      </li>
+      <li>
+        <router-link to="/about/">About</router-link>
+      </li>
+    </ul>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router.js
+++ src/router.js
@@ -16,10 +16,17 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About.vue')
+    },
+    {
+      path: '/inherited-dysfunction',
+      name: 'inherited-dysfunction',
+      component: () => import('./views/InheritedDysfunction.vue')
+    },
+    {
+      path: '/wood-ash',
+      name: 'wood-ash',
+      component: () => import('./views/WoodAsh.vue')
     }
   ]
 })
