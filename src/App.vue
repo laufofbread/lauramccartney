5799d3fe767b1e8a3ccec262b4eb04d65c1fbe91
Laura McCartney <hello@lauramccartney.co.uk>
2019-02-14 15:50:57 +0000

    Add styles from old site

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <h1>
-      <a href="/">Laura McCartney</a>
-    </h1>
-    <ul class="nav">
-      <li>
-        <router-link to="/inherited-dysfunction/">Inherited dysfunction</router-link>
-      </li>
-      <li>
-        <router-link to="/wood-ash/">Wood ash</router-link>
-      </li>
-      <li>
-        <router-link to="/about/">About</router-link>
-      </li>
-    </ul>
+    <nav>
+      <h1 class="home-link">
+        <a href="/">Laura McCartney</a>
+      </h1>
+      <ul class="nav">
+        <li>
+          <router-link to="/inherited-dysfunction/">Inherited dysfunction</router-link>
+        </li>
+        <li>
+          <router-link to="/wood-ash/">Wood ash</router-link>
+        </li>
+        <li>
+          <router-link to="/about/">About</router-link>
+        </li>
+      </ul>
+    </nav>
     <router-view/>
   </div>
 </template>
 
-<style>
-
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre:500');
+  @import "@/styles/_menu.scss";
 </style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+         path.resolve(__dirname, './src/styles/app.scss'),
+         path.resolve(__dirname, './src/styles/_normalize.scss'),
+         path.resolve(__dirname, './src/styles/_main.scss')
+      ]
+    }
+  }
+}
