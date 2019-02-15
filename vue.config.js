979a0d9bb299182360e5d5550c49e0d53d903b82
Laura McCartney <hello@lauramccartney.co.uk>
2019-02-15 22:10:28 +0000

    change publicpath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/lauramccartney/'
+    ? '/lauramccartney.co.uk/'
     : '/',
   pluginOptions: {
     'style-resources-loader': {
