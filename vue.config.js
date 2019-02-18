d81e20be43cf2e191ba337157f71dbc18d8db028
Laura McCartney <hello@lauramccartney.co.uk>
2019-02-18 09:34:51 +0000

    Fix deploy script and add google analytics script

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/lauramccartney.co.uk/'
-    : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
