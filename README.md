18c5ee425b83be13ff08d62aa9a38264a57f98f8
Laura McCartney <hello@lauramccartney.co.uk>
2020-04-06 17:10:45 +0100

    empty directory

--- README.md
+++
@@ -1,4 +0,0 @@
-Laura McCartney's Website
-=========================
-
-This is the GitHub repo for [Laura McCartney's website](http://lauramccartney.co.uk)
--- src/sanity.js
+++
@@ -1,7 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-  projectId: "yth0o5v7",
-  dataset: "production",
-  useCdn: true
-});
