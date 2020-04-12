1b891c57cdace9d35113315fa9d4223e484718aa
Laura McCartney <hello@lauramccartney.co.uk>
2020-04-12 19:44:21 +0100

    begin gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Laura McCartney | Potter`,
+    description: `London Based ceramic artist.`,
+    author: `@lauramccartneyceramics`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "yth0o5v7",
+        dataset: "production",
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        typekit: {
+          id: 'twv1ruz',
+          families: ['nobel']
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from 'gatsby'
+import BlockContent from '@sanity/block-content-to-react'
+import BackgroundImage from 'gatsby-background-image'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const query = graphql`
+  query IndexPageQuery {
+    page: sanityHome {
+      id
+      image {
+        asset {
+          fluid(maxWidth: 2000) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      _rawText
+    }
+  }
+`
+
+const IndexPage = props => {
+  const { data, errors } = props
+  const page = data && data.page
+
+  return (
+    <Layout>
+      <BackgroundImage
+            Tag="section"
+            fluid={page.image.asset.fluid}
+            backgroundColor={`#040e18`}
+          >
+            <BlockContent blocks={page._rawText} />
+      </BackgroundImage>
+      <SEO title="Home" />
+    </Layout>
+  )
+}
+
+
 
 export default IndexPage
