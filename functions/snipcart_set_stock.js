4c4bb5ec59d139dfae62d04495ae5ce37ffd5e51
Laura McCartney <hello@lauramccartney.co.uk>
2020-04-25 14:27:42 +0100

    remove comma

--- functions/snipcart_set_stock.js
+++ functions/snipcart_set_stock.js
@@ -1,5 +1,8 @@
 exports.handler = function(event, context, callback) {
 
+  console.log(event);
+  console.log(context);
+
   if(event.httpMethod !== 'POST') {
     return callback(null, {
       statusCode: 200,
@@ -7,14 +10,13 @@ exports.handler = function(event, context, callback) {
     });
   }
 
-  console.log(event);
-  console.log(context);
+
 
   fetch('https://app.snipcart.com/api/products',
     {
       method:'GET',
       headers:{
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       auth:{
         "user": process.env.SNIPCART_PRIVATE_KEY
