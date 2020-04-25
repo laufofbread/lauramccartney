a1e02f0898dd91af0932906fe5dc995d5d8181f8
Laura McCartney <hello@lauramccartney.co.uk>
2020-04-25 14:04:49 +0100

    Add callback to function

--- functions/snipcart_set_stock.js
+++ functions/snipcart_set_stock.js
@@ -15,5 +15,17 @@ exports.handler = function(event, context, callback) {
         "user": process.env.SNIPCART_PRIVATE_KEY
       },
       body: JSON.stringify(data)
+    }).then((data) => {
+      console.log(event);
+      console.log(context);
+
+      callback(null, {
+      	statusCode: 200,
+      	headers: {
+      		"Content-Type": "application/json"
+      	},
+      	body: JSON.stringify(data),
+      });
+
     })
 }
