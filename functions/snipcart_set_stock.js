06b108dc174af7c60171f432b8aae9c2fdffd7cd
Laura McCartney <hello@lauramccartney.co.uk>
2020-04-28 14:53:14 +0100

    use none user defined id

--- functions/snipcart_set_stock.js
+++ functions/snipcart_set_stock.js
@@ -25,7 +25,7 @@ exports.handler = function(event, context, callback) {
 
         if(!items[i].hasOwnProperty('stock')) {
 
-          fetch(`https://app.snipcart.com/api/${items[i].userDefinedId}`,
+          fetch(`https://app.snipcart.com/api/${items[i].id}`,
             {
               method:'PUT',
               headers:{
